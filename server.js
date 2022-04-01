abea2e9377397b7e81625edfd438606ac2489765
Saheb Dhara <sahebdhara961@gmail.com>
2022-04-01 12:16:49 +0530

    middleware

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const experss = require("express");
 const app = experss();
 
+// middleware 
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 
 const courses = [
@@ -25,6 +28,17 @@ app.get('/api/courses/:id',(req,res)=>{
    if(!course) res.status(404).send('the course with the give ID was wrong ')
    res.send(course);
 });
+
+// post request 
+
+app.post('/api/courses', (req,res)=>{
+    const course = {
+        id:courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+})
 
 
 app.listen(port, ()=> console.log(`the server is running in ${port}`))
