63f214317b120af2547b15466a4a1aa92668f612
Saheb Dhara <sahebdhara961@gmail.com>
2022-04-12 21:57:16 +0530

    connection to mongodb

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require("express")
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config(); // to use env we have to config first 
+
+
+// this is the connection of mongoDB to server 
+mongoose.connect(process.env.MONGO_URL,
+    {useNewUrlParser: true},()=>{
+   console.log("server is connected to mongodb..")
+});
+
+
 
 
 
