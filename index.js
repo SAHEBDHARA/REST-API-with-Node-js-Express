ea1d0e942edc4ee840c7d174526dbf097f60cffb
Saheb Dhara <sahebdhara961@gmail.com>
2022-04-12 22:20:40 +0530

    connect

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/user")
 
 dotenv.config(); // to use env we have to config first 
 
@@ -14,8 +15,25 @@ mongoose.connect(process.env.MONGO_URL,
    console.log("server is connected to mongodb..")
 });
 
+// Middleware 
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+// creating home rout 
+app.get("/",(req,res)=>{
+    res.send("this the homepage")
+})
+
+// creating user rout
+app.get("/user",(req,res)=>{
+    res.send("welcome to user page")
+})
 
 
+// using the routes
+app.use("/api/users",userRoute);
+ 
 
 
 app.listen(3000,()=>{
