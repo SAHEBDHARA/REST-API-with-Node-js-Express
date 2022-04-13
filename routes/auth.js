97e2170dcdd1d6e10e975d619179fec930f4b290
Saheb Dhara <sahebdhara961@gmail.com>
2022-04-14 01:16:09 +0530

    password bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,35 @@
 const router = require("express").Router();
-const User = require("../models/user")
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
-// resister rout
- router.get("/resister", async (req,res)=>{
-    const user = await new User ({
-        username:"saheb",
-        email:"sahebdhara@gmail.com",
-        passowrd:"123456"
-    })
+// resister rout  
+ router.post("/resister", async (req,res)=>{
 
-   await user.save()
-   res.send("ok")
+
+  try
+  {
+      // genetate new password 
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt); // making the password bycript and adding salt
+
+      //create new user
+      const newUser = new User ({
+        username:req.body.username,
+        email:req.body.email,
+        password:hashedPassword,
+    
+      });
+
+
+      // save user and return response
+      const user = await newUser.save() // saveing the data to mongodb
+      res.status(200).json(user) // send the json data to 
+  } 
+  catch(err){
+   console.log(err)
+  }
    
-})
+});
 
 
 module.exports = router;
