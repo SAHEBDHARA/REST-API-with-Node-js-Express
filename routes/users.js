0f86ccedaa977ef8521225b2028ec0f4da2fe848
Saheb Dhara <sahebdhara961@gmail.com>
2022-04-14 14:12:05 +0530

    get follower

--- routes/users.js
+++ routes/users.js
@@ -84,6 +84,31 @@ router.get("/:id", async (req,res)=>{
 
 // follow a user 
 
+router.put("/:id/follow", async (req,res)=>{
+    if(req.body.userId !== req.params.id ){ // if they are not same users
+
+        try{
+            const user = await User.findById(req.params.id); // the user which has the id in the link
+            const currentUser = await User.findById(req.body.userId); // the user who is trying to make request
+
+            if(!user.followers.includes(req.body.userId)){ // if the current user is not following the user
+                await user.updateOne({$push:{followers:req.body.userId}}); // push the id to the followers
+                await currentUser.updateOne({$push:{followings:req.body.userId}}); // same thing for the current user
+                res.status(200).json("the user has been followed")
+
+
+            }else{
+                res.status(403).json("you already follow this user")
+            }
+
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }else{
+        res.status(403).json("you can't follow yourself")
+    }
+})
+
 
 // unfollow a user 
 
